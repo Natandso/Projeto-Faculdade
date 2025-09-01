bc741a5b962d0afbc62064906ee06ecbb7b06f99
Natan Santana (Aero) <minatosantana@hotmail.com>
2025-09-01 17:31:45 -0300

    configurando o setup inicial

---
+++ projeto_faculdade/src/main/java/com/projeto_faculdade/App.java
@@ -0,0 +1,13 @@
+package com.projeto_faculdade;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
